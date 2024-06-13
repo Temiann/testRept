dc0ff171531b4e9f25de0785653d3d29f35e2988
Ilya <temiann36@gmail.com>
2024-06-14 01:42:57 +0300

    added new commit test3

--- src/src/main/java/org/example/Main.java
+++ src/src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("TestRep2!");
         System.out.println("TestRep2!");
         System.out.println("TestRep2!");
+        System.out.println("TestRep2!");
     }
 }
